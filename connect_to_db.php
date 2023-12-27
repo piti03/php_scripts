17cee2ef28ccca15bc4782551c969a2e4c8b65a7
HamidReza Rabipoor <133872879+piti03@users.noreply.github.com>
2023-12-27 12:00:41 -0500

    Update connect_to_db.php

--- connect_to_db.php
+++ connect_to_db.php
@@ -1,6 +1,9 @@
 <?php
+
+
 // config your database
-$con = mysqli_connect("localhost","root","","db_name");
+// mysqli_connect("localhost", "admin_username", "admin_password","db_name")
+$con = mysqli_connect("localhost","root","","db_name"); 
 
 if(mysqli_connect_errno()){
     echo "no a successfull connection". mysqli_connect_error();
